87b216b9f37f04dcb05cb18214afdac774c12bb8
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-17 13:50:43 +0530

    Add : check the abbb pattern in regex

---
+++ RegularExpressionRegax/RegularExpressionRegax/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularExpressionRegax
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RegaxPattern pattern = new RegaxPattern();
+            pattern.MatchingPattern();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegularExpressionRegax/RegularExpressionRegax/RegaxPattern.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressionRegax
+{
+    public class RegaxPattern
+    {
+        public void MatchingPattern()
+        {
+            string input = "ehhh"; // Change this string to test different inputs
+
+            Regex regex = new Regex(@"ab{2,3}");
+
+            if (regex.IsMatch(input))
+            {
+                Console.WriteLine("Match found!");
+            }
+            else
+            {
+                Console.WriteLine("No match found.");
+            }
+        }
+    }
+}
